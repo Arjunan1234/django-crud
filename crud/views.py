dd489807e1a6d801eff1aafa887cb7b16d7beb42
Arjunan <arjunanvijay21@gmail.com>
2023-11-10 19:44:56 +0530

    models created

---
+++ crud/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Student
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def insertData(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        print(name, email, age, gender)
+        query = Student(name=name, email=email, age=age, gender=gender)
+        query.save()
+
+    return render(request, 'index.html')
